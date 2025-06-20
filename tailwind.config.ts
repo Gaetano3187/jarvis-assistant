3226729bfe68e113b41e2f9f13e1c29f76a96c36
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-06-20 03:33:05 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: { extend: {} },
+  plugins: [require('daisyui')],
 };
-
 export default config;
